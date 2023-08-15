57447d4db13ce0311adad52ed0bd9adbad30e35b
Mia Dinh <miadinh38@gmail.com>
2023-08-15 01:49:12 -0400

    Connect to the Server

---
+++ play.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.0.104', // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connected to the server");
+  });
+
+  conn.on("data", (data) => {
+      if(data.includes("you ded cuz you idled")) {
+        console.log("Server message: ", data);
+      }
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
